73c3cec633bd40258dea8c315d39110d6cde8d20
crystalDf <chendong333@gmail.com>
2015-01-13 23:43:12 +0800

    Chapter 26\PhotoGallery AsyncTask

--- app/src/main/java/com/star/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/star/photogallery/PhotoGalleryFragment.java
@@ -1,7 +1,54 @@
 package com.star.photogallery;
 
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.io.IOException;
 
 public class PhotoGalleryFragment extends Fragment {
+
+    private static final String TAG = "PhotoGalleryFragment";
+
+    GridView mGridView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+
+        new FetchItemsTask().execute();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mGridView = (GridView) v.findViewById(R.id.gridView);
+
+        return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl("http://www.baidu.com");
+                Log.i(TAG, "Fetch contents of URL: " + result);
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to fetch URL: ", e);
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 }
